446951b4526ac3eacd02e8192826c73ededcbf63
Nastya <90060338+pwrdpwnz@users.noreply.github.com>
2022-06-04 03:45:50 +0300

    day 2

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -2,11 +2,18 @@ import React from 'react';
 import navStyle from './Nav.module.scss'
 import '../Styles/App.scss'
 
+let per1 = navStyle.active;
+let per2 = navStyle.navLinks;
+
+let x = `$per1 + $per2`
+
 const Nav = () => {
     return (
         <nav className={navStyle.rightNavbar}>
             <ul className={navStyle.rightNavbar__menu}>
-                <li className={navStyle.rightNavbar__items}>My Profile</li>
+                <li className={navStyle.rightNavbar__items}>
+                    <a href="#" className={ `${per1} ${per2}` }>My Profile</a>
+                </li>
                 <li className={navStyle.rightNavbar__items}>My Friends</li>
                 <li className={navStyle.rightNavbar__items}>My Photo</li>
                 <li className={navStyle.rightNavbar__items}>My Music</li>
